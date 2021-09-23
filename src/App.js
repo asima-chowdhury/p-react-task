850cf10a5a968cf59a6a37ff1147d5dfc9a72229
asima-chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2021-09-23 22:47:19 +0600

    about part done & fully responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home/Home";
-import './App.css'
+import './App.css';
+import About from "./components/About/About";
 
 
 function App() {
   return (
     <div>
       <Home />
+      <About/>
     </div>
   );
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './About.css';
+import aboutImg from '../../images/about.png';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="container">
+            <div className="about">
+                <div className="about-gallery">
+                    <img src={aboutImg} alt="aboutImg" />
+                    <img src={aboutImg} alt="aboutImg" />
+                    <img src={aboutImg} alt="aboutImg" />
+                </div>
+                <div className="about-content">
+                    <h1>About Us</h1>
+                    <p>Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will si. The placeholder text used in design when creating content. It helps designers plan out where the content will sitt</p>
+                </div>
+            </div>
         </div>
     );
 };
